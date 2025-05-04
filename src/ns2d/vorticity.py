440f23c72c8a8c330daf1da8f81e2ae9a5b78f9a
Estéban Nocet-Binois <estebannb@tuta.io>
2025-05-03 20:56:50 -0400

    Update vorticity.py

--- src/ns2d/vorticity.py
+++ src/ns2d/vorticity.py
@@ -4,7 +4,7 @@ import numpy as np
 from numba import njit, prange
 from numpy.typing import NDArray
 
-import .core as ns2dcore
+from . import core as ns2dcore
 
 
 def _get_stencil_coefficients(order: int) -> tuple[NDArray[np.float64], int]:
