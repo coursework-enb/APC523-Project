8adf1820f58698fec67532737a802cebf4032d4f
Esteban Nocet-Binois <nocetbinois.esteban@gmail.com>
2025-05-09 01:06:23 -0400

    Experiment 1 written

--- output/exp3.py
+++ output/exp3.py
@@ -1,3 +1,7 @@
+import time
+import itertools
+import json
+
 from ns2d import (
     BaseProjectionSolver,
     FiniteDifferenceDiscretizer,
@@ -16,7 +20,6 @@ CFL_BASED = True
 NX, NY = 41, 41
 DX, DY = 2.0 / (NX - 1), 2.0 / (NY - 1)
 
-
 rhs_options = [
     FiniteDifferenceDiscretizer(),
     FiniteDifferenceUpwindDiscretizer(),
@@ -30,4 +33,47 @@ lhs_options = [
 ]
 nu_options = [1e-3, 1e-5]
 
-# Run for all combinations and get the compute time and error
+results = []
+all_combinations = list(itertools.product(rhs_options, lhs_options, nu_options))
+print(f"Number of configurations to run: {len(all_combinations)}")
+
+for current_ns_discretizer, current_integrator, current_nu in all_combinations:
+    solver = BaseProjectionSolver(
+        nx=NX, ny=NY, dx=DX, dy=DY, dt=0.004, nu=current_nu,
+        integrator=current_integrator,
+        discrete_navier_stokes=current_ns_discretizer,
+        fixed_dt=FIXED_DT
+    )
+
+    try:
+        start_time = time.perf_counter()
+        _, _, error = solver.integrate(
+            num_steps=N_STEPS,
+            end_time=None,
+            benchmark=BENCHMARK,
+            cfl_based=CFL_BASED,
+        )
+        compute_time = time.perf_counter() - start_time
+
+        results.append({
+            "solver": repr(solver),
+            'discretizer': current_ns_discretizer.__class__.__name__,
+            'integrator': current_integrator.__class__.__name__,
+            'nu': current_nu,
+            'compute_time': compute_time,
+            'error': error
+        })
+
+    except Exception as e:
+        print("WARNING: Current combination failed")
+        results.append({
+            "solver": repr(solver),
+            'discretizer': current_ns_discretizer.__class__.__name__,
+            'integrator': current_integrator.__class__.__name__,
+            'nu': current_nu,
+            'error': str(e),
+            'compute_time': None
+        })
+
+with open('results_exp1.json', 'w') as f:
+    json.dump(results, f, indent=4)
